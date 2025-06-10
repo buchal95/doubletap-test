9ac9b041b10e3807410487b8ec415f06ec5ff23e
buchal95 <81154784+buchal95@users.noreply.github.com>
2025-06-10 18:38:18 +0200

    Implement Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -119,6 +119,18 @@ export default function RootLayout({
   return (
     <html lang="cs" className={`${anton.variable} ${montserrat.variable}`}>
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M3ZNVD4K');`
+          }}
+        />
+        {/* End Google Tag Manager */}
+        
         <link rel="icon" type="image/png" sizes="16x16" href="/fav16.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/fav32.png" />
         <link rel="shortcut icon" href="/fav16.png" />
@@ -206,6 +218,17 @@ export default function RootLayout({
         />
       </head>
       <body className="font-montserrat text-brand-gray antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M3ZNVD4K"
+            height="0" 
+            width="0" 
+            style={{display:'none', visibility:'hidden'}}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        
         {children}
       </body>
     </html>
